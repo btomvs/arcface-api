6fd984f2dfb78b727f865d22368efc7346799737
Brayan Chávez Riveros <101909261+btomvs@users.noreply.github.com>
2025-11-25 00:11:52 -0300

    Update start.py

--- start.py
+++ start.py
@@ -2,15 +2,16 @@
 import os
 import uvicorn
 
+
 def main():
     port = int(os.getenv("PORT", "8080"))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
         port=port,
-        # opcional: más workers si quieres
-        # workers=1,
+        # workers=1,  # opcional
     )
+
 
 if __name__ == "__main__":
     main()
